0618ed826035b8d3895e0443b6b74266a44a015d
Shaiyo <edsonmike053@gmail.com>
2024-03-13 17:01:31 +0300

    Framer Motion Tutoria

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
+function App() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  return (
+    <>
+      <div className="grid place-content-center h-screen gap-2">
+        {/* <h1 className="bg-blue-500 text-3xl p-5 rounded-md">
+          Tailwind X React
+        </h1> */}
+        <motion.button
+          className=" bg-blue-600 rounded-md text-white p-2 mb-14"
+          onClick={() => setIsVisible(!isVisible)}
+          layout
+        >
+          Show/Hide
+        </motion.button>
+
+        <AnimatePresence mode="popLayout">
+          {isVisible && (
+            <motion.div
+              initial={{ rotate: "0deg", scale: 0, y: 0 }}
+              animate={{
+                rotate: "180deg",
+                scale: 1,
+                y: [0, 50, -50, -50, 0],
+              }}
+              exit={{
+                rotate: "0deg",
+                scale: 0,
+                y: 0,
+              }}
+              transition={{
+                duration: 1,
+                ease: "backInOut",
+                times: [0, 0.25, 0.5, 0.85, 1],
+              }}
+              className=" w-40 h-40 bg-black"
+            ></motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Gestures.tsx
@@ -0,0 +1,12 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
+function Gestures() {
+  return (
+    <>
+      <div className="grid place-content-center h-screen gap-2"></div>
+    </>
+  );
+}
+
+export default Gestures;
